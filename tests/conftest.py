d24fca527d6afc8be0ec15e2c262f2a688b951e4
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-07-24 11:24:03 +0200

    Drop authorization headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,10 @@ import pytest
 @pytest.fixture
 def anyio_backend():
     return "asyncio"
+
+
+@pytest.fixture
+def vcr_config():
+    return {
+        "filter_headers": [("authorization", None)],
+    }
