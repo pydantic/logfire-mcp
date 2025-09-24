5ec06b73f2daceb046b69b4b096701140be6a6ff
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-09-24 09:35:25 -0700

    Add `schema_reference` tool

--- logfire_mcp/state.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from logfire.experimental.query_client import AsyncLogfireQueryClient
-
-
-@dataclass
-class MCPState:
-    logfire_client: AsyncLogfireQueryClient
